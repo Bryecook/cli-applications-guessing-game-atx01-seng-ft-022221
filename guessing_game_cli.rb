baba083163bbd0b97e635c171c47f1d28897883b
Brian Cook <brybcook@gmail.com>
2021-02-21 17:32:19 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ require "pry"
 
 def run_guessing_game
   puts "Guess a number between 1 and 6"
-  computer_number==rand(1..6)
+  computer_number=rand(1..6)
   input=gets.chomp
   if input == "exit"
   return "Goodbye!"
