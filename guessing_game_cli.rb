b54fb6bb3a62fc070b73d9ac42e57f4965f74afb
Brian Cook <brybcook@gmail.com>
2021-02-21 17:31:31 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,8 +7,9 @@ def run_guessing_game
   input=gets.chomp
   if input == "exit"
   return "Goodbye!"
-    puts "Sorry! The computer guessed 6."
   elsif input.to_i == computer_number
     puts "You guessed the correct number!"
+  else
+    puts "Sorry! The computer guessed 6."
 end
 end
